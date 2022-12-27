2c815002832475a9e89ff670cf879353638a22d3
shreshth-29 <77191933+shreshth-29@users.noreply.github.com>
2022-12-27 17:09:09 +0530

    Update sh_albert_cls.sh

--- transformer-mrc/sh_albert_cls.sh
+++ transformer-mrc/sh_albert_cls.sh
@@ -1,9 +1,9 @@
 #sketchy module
-export DATA_DIR=data
+export DATA_DIR=/content/AwesomeMRC/transformer-mrc/examples/tests_samples/squad_test
 export TASK_NAME=squad
 python ./examples/run_cls.py \
     --model_type albert \
-    --model_name_or_path albert-xxlarge-v2 \
+    --model_name_or_path albert-base-v2 \
     --task_name $TASK_NAME \
     --do_train \
     --do_eval \
@@ -16,6 +16,6 @@ python ./examples/run_cls.py \
     --learning_rate 2e-5 \
     --num_train_epochs 2.0 \
     --eval_all_checkpoints \
-    --output_dir squad/cls_squad2_albert-xxlarge-v2_lr2e-5_len512_bs48_ep2_wm814_fp16 \
+    --output_dir squad/cls_squad2_albert-base-v2_lr2e-5_len512_bs48_ep2_wm814_fp16 \
     --save_steps 2500 \
     --fp16
